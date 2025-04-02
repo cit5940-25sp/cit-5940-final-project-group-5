443476a54d5efa546a54e4ab2956efb12b22d94c
Charlie Cheng <122961037+Charlietscr7@users.noreply.github.com>
2025-04-02 17:53:12 -0400

    Set up

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static String sayHello() {
-        return "Hello," + " World!";
+        return "Hello," + " Harry!";
     }
 
     public static void main(String[] args) {
